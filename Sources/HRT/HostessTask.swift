87f89a051596c558ca77d55867340300b064ea8c
Ky Northstar <Ky@KyNorthstar.me>
2026-01-09 01:19:39 -0700

    Added a runtime-only summary type for task completion

--- Sources/HRT/HostessTask.swift
+++ Sources/HRT/HostessTask.swift
@@ -38,16 +38,103 @@ public struct HostessTask {
     
     /// A list of IDs of tags which apply to this task
     public var tags: [ShelfId]? = nil
+    
     public var state: State? = nil
+    
+    /// How complete is the current task? `0.0`~`1.0`
     public var completionPercentage: CGFloat? = nil
 }
 
 
 
-extension HostessTask {
-    public enum State: String {
+public extension HostessTask {
+    /// The current broad state of this task
+    enum State: String {
+        /// The task has been created and can currently be worked on
         case open
+        
+        /// The task has been completed, successfully or not
         case complete
+        
+        /// The task was not completed and is no longer going to be worked on
+        case dropped
+    }
+}
+
+
+
+// MARK: - Conveniences
+
+public extension HostessTask {
+    var completion: Completion {
+        get {
+            guard let state else {
+                if let completionPercentage {
+                    return .inProgress(percentage: completionPercentage)
+                }
+                else {
+                    return .notStarted
+                }
+            }
+            
+            switch state {
+            case .open:
+                if let completionPercentage,
+                   completionPercentage > 0
+                {
+                    if completionPercentage > 1 {
+                        return .complete
+                    }
+                    else {
+                        return .inProgress(percentage: completionPercentage)
+                    }
+                }
+                else {
+                    return .notStarted
+                }
+                
+            case .complete: return .complete
+            case .dropped: return .dropped
+            }
+        }
+        
+        set {
+            switch newValue {
+            case .notStarted:
+                self.state = .open
+                self.completionPercentage = nil
+                
+            case .inProgress(percentage: let percentage):
+                self.state = .open
+                self.completionPercentage = percentage
+                
+            case .complete:
+                self.state = .complete
+                self.completionPercentage = 1
+                
+            case .dropped:
+                self.state = .dropped
+                self.completionPercentage = nil
+            }
+        }
+    }
+    
+    
+    
+    /// How complete this task is
+    enum Completion {
+        
+        /// The task hasn't yet been started
+        case notStarted
+        
+        /// The task is currently being worked on
+        /// - Parameter percentage: How far along is the progress? `0.0`~`1.0`
+        case inProgress(percentage: CGFloat)
+        
+        /// The task is fully complete (or at least complete enough to be marked as complete)
+        case complete
+        
+        /// The task is incomplete but will not be worked on
         case dropped
     }
 }
