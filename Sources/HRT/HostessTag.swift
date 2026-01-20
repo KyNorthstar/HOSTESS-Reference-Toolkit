b90c0c3d5e4ac617cfa066e0c24ff6e53b0fdc6b
Ky Northstar <Ky@KyNorthstar.me>
2026-01-20 12:16:11 -0700

    Implementing HOSTESS tags

---
+++ Sources/HRT/HostessTag.swift
@@ -0,0 +1,35 @@
+//
+//  HostessTag.swift
+//  HOSTESS Reference Toolkit
+//
+//  Created by Ky on 2026-01-04.
+//
+
+import Foundation
+
+import SHELF
+
+
+
+/// A tag in HOSTESS
+public struct HostessTag {
+    
+    /// Universally identifies this task 
+    public var id: ShelfId = .init()
+    
+    
+    /// The label text of the tag
+    public var label: String
+    
+    
+    public init(id: ShelfId = .init(), label: String) {
+        self.id = id
+        self.label = label
+    }
+}
+
+
+
+// MARK: - Conformances
+
+extension HostessTag: HostessObjectIdealPayload {}
