a0ae86afcf62504fc7e518209698086bf71362e5
Ky <Ky@KyNorthstar.me>
2026-06-05 01:54:51 -0600

    Custom HOSTESS object kinds!
    I decided on reverse-domain-name notation, with HOSTESS-native objects being the exception. This should leave the door open for infinite scaling for both.

--- Sources/HRT/Objects/Serialization wrapper/HostessStorageWrapper.swift
+++ Sources/HRT/Objects/Serialization wrapper/HostessStorageWrapper.swift
@@ -68,11 +68,13 @@ public extension HostessStorageWrapper {
 // MARK: - Subtypes
 
 /// Describes various types of HOSTESS object.
-///
-/// The format (e.g. Swift type) of `payload` fields in ``HostessObject``s should align to this type
-public enum HostessObjectKind: String {
+/// 
+/// The format (e.g. Swift type) of `payload` fields in ``HostessObject``s should align to this type.
+public enum HostessObjectKind {
     
     /// The prototypical HOSTESS object: a single task
+    ///
+    /// A todo, a grocery, an appointment, an email, etc.
     case task
     
     /// A collection of related tasks.
@@ -84,7 +86,127 @@ public enum HostessObjectKind: String {
     case tag
     
     
-    // TODO: How do we represent third-party objects?
+    /// Any arbitrary object, stored within HOSTESS
+    case custom(RawValue)
+    
+    
+    /// All the non-custom HOSTESS object kinds
+    static let reservedCases: [Self] = [.task, .tasklist, .tag]
+}
+
+
+
+extension HostessObjectKind: RawRepresentable {
+    
+    
+    public init(rawValue: RawValue) {
+        switch rawValue {
+        case Self.task.rawValue:     self = .task
+        case Self.tasklist.rawValue: self = .tasklist
+        case Self.tag.rawValue:      self = .tag
+        default:                     self = .custom(rawValue)
+        }
+    }
+    
+    
+    public var rawValue: RawValue {
+        switch self {
+        case .task:                 return RawValue(hostessReservedType: "task")
+        case .tasklist:             return RawValue(hostessReservedType: "tasklist")
+        case .tag:                  return RawValue(hostessReservedType: "tag")
+        case .custom(let rawValue): return rawValue
+        }
+    }
+    
+    
+    
+    /// The raw value of a HOSTESS object kind.
+    ///
+    /// - Attention: The fields of this raw value MUST be in ASCII alphanumerics. If they contain any other characters, this will refuse to initialize.
+    public struct RawValue: Equatable, Sendable {
+        
+        /// The domain of the type.
+        ///
+        /// HOSTESS reserved system objects have an empty domain. All others MUST be a traditional reverse-domain-name-formatted ASCII alphanumeric dot-separated string, like `"org.kindness.app"`, and SHOULD be all-lowercase.
+        public let domain: String
+        
+        /// The type name
+        ///
+        /// This MUST be ASCII alphanumeric, like `"Chart"`. It SHOULD be capitalized-camel-case.
+        public let type: String
+        
+        
+        /// Initialize a custom type's raw value
+        ///
+        /// - Parameters:
+        ///   - domain: The domain of the type.
+        ///             This MUST be a traditional reverse-domain-name-formatted ASCII alphanumeric dot-separated string, like `"org.kindness.app"`. It SHOULD be all-lowercase.
+        ///
+        ///   - type:   The type name itself.
+        ///             This MUST be ASCII alphanumeric, like `"Chart"`. It SHOULD be capitalized-camel-case.
+        public init?(domain: String, type: String) {
+            let domainRegex = /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)+$/
+            let typeRegex = /^[a-zA-Z0-9]+$/
+            
+            guard nil != (try? domainRegex.firstMatch(in: domain)),
+                  nil != (try? typeRegex.firstMatch(in: type))
+            else {
+                return nil
+            }
+            
+            self.domain = domain
+            self.type = type
+        }
+        
+        
+        fileprivate init(hostessReservedType: String) {
+            self.domain = ""
+            self.type = hostessReservedType
+        }
+    }
+}
+
+
+
+extension HostessObjectKind.RawValue: CustomStringConvertible {
+    public var description: String {
+        "\(domain).\(type)"
+    }
+}
+
+
+
+extension HostessObjectKind.RawValue: Encodable {
+    public func encode(to encoder: any Encoder) throws {
+        var container: SingleValueEncodingContainer = encoder.singleValueContainer()
+        try container.encode(description)
+    }
+}
+
+
+
+extension HostessObjectKind.RawValue: Decodable {
+    public init(from decoder: any Decoder) throws {
+        let container: SingleValueDecodingContainer = try decoder.singleValueContainer()
+        
+        let rawValueString: String = try container.decode(String.self)
+        
+        for reserved in HostessObjectKind.reservedCases {
+            if reserved.rawValue.description == rawValueString {
+                self = reserved.rawValue
+                return
+            }
+        }
+        
+        let regex = /^(?<domain>[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)+)\.(?<type>[a-zA-Z0-9]+)$/
+        
+        guard let match = try regex.firstMatch(in: rawValueString)?.output else {
+            throw DecodingError.dataCorrupted(DecodingError.Context.init(codingPath: [], debugDescription: #"Custom HOSTESS object types MUST be in the format /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)+[a-zA-Z0-9]+$/ – the given value was "\#(rawValueString)""#))
+        }
+        
+        self.domain = String(match.domain)
+        self.type = String(match.type)
+    }
 }
 
 
@@ -101,7 +223,26 @@ extension HostessStorageWrapper: AnyHostessType {}
 extension HostessStorageWrapper: ShelfData {}
 
 extension HostessObjectKind: AnyHostessType {}
-extension HostessObjectKind: Codable {}
+
+
+
+extension HostessObjectKind: Encodable {
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(self.rawValue)
+    }
+}
+
+
+
+extension HostessObjectKind: Decodable {
+    public init(from decoder: any Decoder) throws {
+        self.init(rawValue: try decoder
+            .singleValueContainer()
+            .decode(RawValue.self)
+        )
+    }
+}
 
 
 
